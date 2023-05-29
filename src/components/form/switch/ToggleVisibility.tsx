b65cda649ee3a49c8c81017395124fccee1535ed
Pedro Oliveira <pedro.oliveira@ssp.df.gov.br>
2023-05-29 12:17:06 -0300

    [v68] done - togglevisibility

--- src/components/form/switch/ToggleVisibility.tsx
+++ src/components/form/switch/ToggleVisibility.tsx
@@ -1,13 +1,14 @@
 import React, { useContext, useEffect } from 'react'
 import { FormContext } from '../../../context/form'
+import { invert } from 'lodash'
 
 // Coloque esse componente dentro de um bloco que é retirado com o valor do input
-export function ToggleVisibility(props: { switchId: string; unregisterNameList: string[] }) {
+export function ToggleVisibility({ invert = false, ...props }: { switchId: string; unregisterNameList: string[]; invert?: boolean }) {
     const context = useContext(FormContext)!
 
     useEffect(() => {
         return () => {
-            if (context.formWatch(props.switchId)) {
+            if (context.formWatch(props.switchId) === invert) {
                 props.unregisterNameList.forEach((x) => context.formUnregister(x))
             }
         }
@@ -21,7 +22,7 @@ export function SwitchWatch(props: { children: JSX.Element | JSX.Element[]; swit
 
     return (
         <>
-            {!context?.formWatch(props.switchId) && (
+            {context?.formWatch(props.switchId) !== invert && (
                 <>
                     <ToggleVisibility switchId={props.switchId} unregisterNameList={props.unregisterNameList} />
                     {props.children}
