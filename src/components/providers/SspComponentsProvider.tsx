ff95dc7a6e075348a6247307dc4ac0650339765b
Pedro Oliveira <pedro.oliveira@ssp.df.gov.br>
2024-02-28 16:13:01 -0300

    v190 - retirado importacao css

--- src/components/providers/SspComponentsProvider.tsx
+++ src/components/providers/SspComponentsProvider.tsx
@@ -3,7 +3,7 @@ import { ToastContainer } from 'react-toastify'
 
 import { CustomModalProvider } from '../modal/Modal'
 
-import 'react-toastify/dist/ReactToastify.css'
+// import 'react-toastify/dist/ReactToastify.css'
 
 //components principal da aplicação
 export function SspComponentsProvider(props: { children: JSX.Element | JSX.Element[] }) {
